16010bd4ee35aa4726c66e8d64a18f4ee35d1a01
saobangmath <anhtai2672000@gmail.com>
2020-09-01 13:25:41 +0800

    port listening configuration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // configure the express server to listen on port 3000;
 server.listen(port, () => {
